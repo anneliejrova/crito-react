0c9765efc92f58dc1713fa52992f3e7e8dee7eb6
Annelie <annelie.j.rova@gmail.com>
2023-11-17 12:22:57 +0100

    finnishing touches G

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import './App.scss'
+import './App.css'
 import Home from './views/Home'
 import Contact from './views/Contact'
 import News from './views/News'
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import PageTitle from '../components/PageTitle'
 import ContactUs from '../components/ContactUs'
 import ContactForm from '../components/ContactForm'
-<<<<<<< HEAD
 import Map from '../components/Map'
-=======
->>>>>>> 38438c885d959d4292be91c2abd388df2aa9cd7e
 
 const Contact = () => {
   return (
@@ -14,14 +11,8 @@ const Contact = () => {
         <PageTitle title="Contact" currentUrl='/contact' currentPage='Contact' />
         <ContactUs />
         <ContactForm />
-<<<<<<< HEAD
         <Map />
       </main>
-=======
-
-        <div className='center-content'><br /> <br /> <br /> <br /><b>Add Mapp here! </b><br /> <br /><br /> <br /></div>
-        </main>
->>>>>>> 38438c885d959d4292be91c2abd388df2aa9cd7e
   )
 }
 
